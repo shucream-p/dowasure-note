48ce3921e69567ac346f8ec18ec9cfc0e8dc224a
tominaga <aurora25sh.50nonono@gmail.com>
2023-10-25 19:19:41 +0900

    Memoのモデルテストを作成した

--- spec/models/memo_spec.rb
+++ spec/models/memo_spec.rb
@@ -3,5 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe Memo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー単位では重複した名前は許可しないこと' do
+    let(:user) { FactoryBot.create(:user) }
+
+    context '同じユーザーの場合' do
+      it '登録失敗すること' do
+        Memo.create(user:, name: 'ど忘れしやすい名前')
+
+        memo = Memo.new(user:, name: 'ど忘れしやすい名前')
+        memo.valid?
+        expect(memo.errors[:name]).to include('has already been taken')
+      end
+    end
+
+    context '異なるユーザーの場合' do
+      it '登録成功すること' do
+        Memo.create(
+          user: FactoryBot.create(:user, email: 'testuser1@example.com'),
+          name: 'ど忘れしやすい名前'
+        )
+
+        memo = Memo.new(
+          user: FactoryBot.create(:user, email: 'testuser2@example.com'),
+          name: 'ど忘れしやすい名前'
+        )
+        expect(memo).to be_valid
+      end
+    end
+  end
 end
