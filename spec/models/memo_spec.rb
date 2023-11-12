599227225c6cc8968a588deeaf2be59adf37719e
tominaga <aurora25sh.50nonono@gmail.com>
2023-11-12 11:53:58 +0900

    increase_search_countメソッドのテストを作成した

--- spec/models/memo_spec.rb
+++ spec/models/memo_spec.rb
@@ -43,4 +43,22 @@ RSpec.describe Memo, type: :model do
       end
     end
   end
+
+  describe '#increase_search_count' do
+    let(:memo) { create(:memo) }
+
+    it 'search_countが1増えること' do
+      expect(memo.search_count).to eq 0
+
+      memo.increase_search_count
+      expect(memo.search_count).to eq 1
+    end
+
+    it 'updated_atが更新されないこと' do
+      updated_at = memo.updated_at
+
+      memo.increase_search_count
+      expect(memo.updated_at).to eq updated_at
+    end
+  end
 end
