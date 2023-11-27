e8e32af6ccd724c56a3276e80665ba247528a764
tominaga <aurora25sh.50nonono@gmail.com>
2023-11-27 15:15:55 +0900

    ユーザーアイコンクリックでログアウトボタンを表示するようにした

--- app/javascript/fontawesome.js
+++ app/javascript/fontawesome.js
@@ -1,4 +1,5 @@
 import "@fortawesome/fontawesome-free";
 import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { faCommentDots } from "@fortawesome/free-regular-svg-icons";
-library.add(faCommentDots);
+library.add(faUser, faCommentDots);
