ae623294ae62f86b35e2cff5f2202d730f303e5a
tominaga <aurora25sh.50nonono@gmail.com>
2023-12-17 12:58:32 +0900

    キーワードタグの説明をモーダルで表示するようにした

--- app/javascript/fontawesome.js
+++ app/javascript/fontawesome.js
@@ -2,8 +2,9 @@ import "@fortawesome/fontawesome-free";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import {
+  faCircleQuestion,
   faCommentDots,
   faPenToSquare,
   faTrashCan,
 } from "@fortawesome/free-regular-svg-icons";
-library.add(faUser, faCommentDots, faPenToSquare, faTrashCan);
+library.add(faUser, faCircleQuestion, faCommentDots, faPenToSquare, faTrashCan);
