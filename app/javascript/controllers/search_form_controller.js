d96411f539b1b5bd46cc982884c122218765318c
tominaga <aurora25sh.50nonono@gmail.com>
2023-12-23 14:17:44 +0900

    テキストを入力するまでリクエストを送信しないよう修正した

--- app/javascript/controllers/search_form_controller.js
+++ app/javascript/controllers/search_form_controller.js
@@ -2,11 +2,13 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="search-form"
 export default class extends Controller {
-  submit() {
-    clearTimeout(this.timeout);
+  submit(event) {
+    if (event.data !== undefined) {
+      clearTimeout(this.timeout);
 
-    this.timeout = setTimeout(() => {
-      this.element.requestSubmit();
-    }, 200);
+      this.timeout = setTimeout(() => {
+        this.element.requestSubmit();
+      }, 200);
+    }
   }
 }
