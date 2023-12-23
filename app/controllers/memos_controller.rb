b3c3116d781799e81aeb3e4d585a88fdf0fdd549
tominaga <aurora25sh.50nonono@gmail.com>
2023-12-23 13:42:23 +0900

    検索時のメモをど忘れしやすい順に表示するようにした

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -47,9 +47,11 @@ class MemosController < ApplicationController
     memos = current_user.memos
     @memos = if params[:q].present?
                memos.search(params[:q], params[:search_option])
+                    .forgetful
                     .page(params[:page])
              else
-               memos.page(params[:page])
+               memos.forgetful
+                    .page(params[:page])
              end
 
     @memos.each(&:increase_search_count) if params[:q].present?
