f6d1e985043b58db7cf3aa68262424715479d19e
tominaga <aurora25sh.50nonono@gmail.com>
2024-01-22 15:04:27 +0900

    不要なバリデーションテストを削除した

--- spec/models/memo_spec.rb
+++ spec/models/memo_spec.rb
@@ -3,31 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe Memo, type: :model do
-  describe 'ユーザー単位では重複した名前は許可しないこと' do
-    let(:user) { create(:user, email: 'testuser@example.com') }
-
-    context '同じユーザーの場合' do
-      it '登録失敗すること' do
-        create(:memo, user:, name: 'ど忘れしやすい名前')
-
-        memo = build(:memo, user:, name: 'ど忘れしやすい名前')
-        memo.valid?
-        expect(memo.errors[:name]).to include('はすでに存在します')
-      end
-    end
-
-    context '異なるユーザーの場合' do
-      it '登録成功すること' do
-        other_user = create(:user, email: 'otheruser@test.com')
-
-        create(:memo, user:, name: 'ど忘れしやすい名前')
-
-        memo = build(:memo, user: other_user, name: 'ど忘れしやすい名前')
-        expect(memo).to be_valid
-      end
-    end
-  end
-
   describe '.search' do
     let(:memo) { create(:memo, name: 'ど忘れしやすい名前', tag_list: %w[タグ1 タグ2]) }
 
