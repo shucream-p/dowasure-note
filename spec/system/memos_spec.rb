023d79462f23d74e429368ae97234ca878a83f91
tominaga <aurora25sh.50nonono@gmail.com>
2024-01-27 11:12:39 +0900

    describeを整理した

--- spec/system/memos_spec.rb
+++ spec/system/memos_spec.rb
@@ -9,16 +9,11 @@ RSpec.describe 'Memos', type: :system do
     sign_in user
   end
 
-  describe 'メモの表示' do
-    before do
-      create(:memo, name: 'ど忘れしやすい名前', tag_list: %w[タグ1 タグ2], search_count: 1, user:)
-      create(:memo, name: '覚えておきたい名前', tag_list: ['タグ1'], user:)
+  describe '一覧表示' do
+    it 'メモが表示されること' do
+      create(:memo, name: 'ど忘れしやすい名前', user:)
       visit root_path
-    end
-
-    it '全てのメモが表示されること' do
       expect(page).to have_content 'ど忘れしやすい名前'
-      expect(page).to have_content '覚えておきたい名前'
     end
 
     it '他ユーザーのメモは表示されないこと' do
@@ -28,138 +23,158 @@ RSpec.describe 'Memos', type: :system do
       visit root_path
       expect(page).to have_no_content '他ユーザーのメモ'
     end
+  end
 
-    describe '検索時の表示' do
-      context 'キーワードタグで検索した場合' do
-        it '「ど忘れしやすい名前」のみ表示されること' do
-          expect(page).to have_checked_field('キーワードタグ')
-          fill_in 'q', with: 'タグ1 タグ2'
-          find 'turbo-frame[complete]'
-          expect(page).to have_content 'ど忘れしやすい名前'
-          expect(page).to have_no_content '覚えておきたい名前'
-        end
-      end
+  describe '内容表示' do
+    it 'メモの内容を表示できること' do
+      create(:memo, content: 'ど忘れしやすい名前です', user:)
 
-      context '名前で検索した場合' do
-        it '「覚えておきたい名前」のみ表示されること' do
-          choose '名前'
-          fill_in 'q', with: '覚えて'
-          find 'turbo-frame[complete]'
-          expect(page).to have_content '覚えておきたい名前'
-          expect(page).to have_no_content 'ど忘れしやすい名前'
-        end
-      end
+      visit root_path
+      comment_icon = find '.fa-comment-dots'
+      comment_icon.click
+      expect(page).to have_content 'ど忘れしやすい名前です'
+    end
+  end
 
-      context '検索ワードを削除した場合' do
-        it '全てのメモが表示されること' do
-          fill_in 'q', with: 'タグ1 タグ2'
-          find 'turbo-frame[complete]'
-          expect(page).to have_content 'ど忘れしやすい名前'
-          expect(page).to have_no_content '覚えておきたい名前'
-          find('input[name="q"]').send_keys([:backspace] * 7)
-          find 'turbo-frame[complete]'
-          expect(page).to have_content 'ど忘れしやすい名前'
-          expect(page).to have_content '覚えておきたい名前'
-        end
-      end
+  describe '登録' do
+    it 'メモを登録できること' do
+      visit root_path
+      click_button '登録する'
+      fill_in 'memo[name]', with: 'ど忘れしやすい名前'
+      tag_input = find '.tagify__input'
+      tag_input.set 'タグ1'
+      tag_input.send_keys :enter
 
-      context '検索結果なしの場合' do
-        it '「見つかりませんでした。」と表示されること' do
-          fill_in 'q', with: 'タグ3'
-          expect(page).to have_content '見つかりませんでした。'
-        end
+      # タグが保存用のフォーマットに変換され、セットされているかの確認
+      expect(page).to have_field 'memo[tag_list]', with: 'タグ1', visible: false
+
+      fill_in 'memo[content]', with: 'ど忘れしやすい名前です'
+      click_button '登録'
+      expect(page).to have_content 'メモが作成されました'
+      expect(page).to have_content 'ど忘れしやすい名前'
+
+      # スワイプしてリンクを表示させる
+      slider = find '.swiper-wrapper'
+      execute_script("arguments[0].style.transform = 'translateX(-85.5px)';", slider)
+
+      edit_icon = find '.fa-pen-to-square'
+      edit_icon.click
+      expect(page).to have_content 'タグ1'
+      expect(page).to have_content 'ど忘れしやすい名前です'
+    end
+  end
+
+  describe '編集' do
+    it 'メモを編集できること' do
+      create(:memo, name: 'ど忘れしやすい名前', user:)
+
+      visit root_path
+
+      # スワイプしてリンクを表示させる
+      slider = find '.swiper-wrapper'
+      execute_script("arguments[0].style.transform = 'translateX(-85.5px)';", slider)
+
+      edit_icon = find '.fa-pen-to-square'
+      edit_icon.click
+      expect(page).to have_field 'memo[name]', with: 'ど忘れしやすい名前'
+      fill_in 'memo[name]', with: '編集した名前'
+      click_button '更新'
+      expect(page).to have_content 'メモが更新されました'
+      expect(page).to have_content '編集した名前'
+    end
+  end
+
+  describe '削除' do
+    it 'メモを削除できること' do
+      create(:memo, name: 'ど忘れしやすい名前', user:)
+
+      visit root_path
+      expect(page).to have_content 'ど忘れしやすい名前'
+
+      # スワイプしてリンクを表示させる
+      slider = find '.swiper-wrapper'
+      execute_script("arguments[0].style.transform = 'translateX(-85.5px)';", slider)
+
+      delete_icon = find '.fa-trash-can'
+      delete_icon.click
+      expect(page.accept_confirm).to eq '本当に削除してよろしいですか？'
+      expect(page).to have_content 'メモが削除されました'
+      expect(page).to have_content 'まだメモが登録されていません'
+    end
+  end
+
+  describe '検索' do
+    before do
+      create(:memo, name: 'ど忘れしやすい名前', tag_list: %w[タグ1 タグ2], user:)
+      create(:memo, name: '覚えておきたい名前', tag_list: ['タグ1'], user:)
+      visit root_path
+    end
+
+    context 'キーワードタグで検索した場合' do
+      it '「ど忘れしやすい名前」のみ表示されること' do
+        expect(page).to have_checked_field('キーワードタグ')
+        fill_in 'q', with: 'タグ1 タグ2'
+        find 'turbo-frame[complete]'
+        expect(page).to have_content 'ど忘れしやすい名前'
+        expect(page).to have_no_content '覚えておきたい名前'
       end
     end
 
-    describe 'ソート機能' do
-      it '検索した後順番が入れ替わること' do
-        expect(first('#name').text).to eq 'ど忘れしやすい名前'
+    context '名前で検索した場合' do
+      it '「覚えておきたい名前」のみ表示されること' do
         choose '名前'
         fill_in 'q', with: '覚えて'
         find 'turbo-frame[complete]'
-        visit root_path
-        expect(first('#name').text).to eq '覚えておきたい名前'
+        expect(page).to have_content '覚えておきたい名前'
+        expect(page).to have_no_content 'ど忘れしやすい名前'
       end
+    end
 
-      it 'ソートのリンクが機能すること' do
-        expect(first('#name').text).to eq 'ど忘れしやすい名前'
-        click_link('更新順')
+    context '検索ワードを削除した場合' do
+      it '全てのメモが表示されること' do
+        fill_in 'q', with: 'タグ1 タグ2'
         find 'turbo-frame[complete]'
-        expect(first('#name').text).to eq '覚えておきたい名前'
-        click_link('ど忘れが多い順')
+        expect(page).to have_content 'ど忘れしやすい名前'
+        expect(page).to have_no_content '覚えておきたい名前'
+        find('input[name="q"]').send_keys([:backspace] * 7)
         find 'turbo-frame[complete]'
-        expect(first('#name').text).to eq 'ど忘れしやすい名前'
+        expect(page).to have_content 'ど忘れしやすい名前'
+        expect(page).to have_content '覚えておきたい名前'
+      end
+    end
+
+    context '検索結果なしの場合' do
+      it '「見つかりませんでした。」と表示されること' do
+        fill_in 'q', with: 'タグ3'
+        expect(page).to have_content '見つかりませんでした。'
       end
     end
   end
 
-  it 'メモを登録できること' do
-    visit root_path
-    click_button '登録する'
-    fill_in 'memo[name]', with: 'ど忘れしやすい名前'
-    tag_input = find '.tagify__input'
-    tag_input.set 'タグ1'
-    tag_input.send_keys :enter
+  describe 'ソート' do
+    before do
+      create(:memo, name: 'ど忘れしやすい名前', search_count: 1, user:)
+      create(:memo, name: '覚えておきたい名前', user:)
+      visit root_path
+    end
 
-    # タグが保存用のフォーマットに変換され、セットされているかの確認
-    expect(page).to have_field 'memo[tag_list]', with: 'タグ1', visible: false
+    it '検索した後順番が入れ替わること' do
+      expect(first('#name').text).to eq 'ど忘れしやすい名前'
+      choose '名前'
+      fill_in 'q', with: '覚えて'
+      find 'turbo-frame[complete]'
+      visit root_path
+      expect(first('#name').text).to eq '覚えておきたい名前'
+    end
 
-    fill_in 'memo[content]', with: 'ど忘れしやすい名前です'
-    click_button '登録'
-    expect(page).to have_content 'メモが作成されました'
-    expect(page).to have_content 'ど忘れしやすい名前'
-
-    # スワイプしてリンクを表示させる
-    slider = find '.swiper-wrapper'
-    execute_script("arguments[0].style.transform = 'translateX(-85.5px)';", slider)
-
-    edit_icon = find '.fa-pen-to-square'
-    edit_icon.click
-    expect(page).to have_content 'タグ1'
-    expect(page).to have_content 'ど忘れしやすい名前です'
-  end
-
-  it 'メモを編集できること' do
-    create(:memo, name: 'ど忘れしやすい名前', user:)
-
-    visit root_path
-
-    # スワイプしてリンクを表示させる
-    slider = find '.swiper-wrapper'
-    execute_script("arguments[0].style.transform = 'translateX(-85.5px)';", slider)
-
-    edit_icon = find '.fa-pen-to-square'
-    edit_icon.click
-    expect(page).to have_field 'memo[name]', with: 'ど忘れしやすい名前'
-    fill_in 'memo[name]', with: '編集した名前'
-    click_button '更新'
-    expect(page).to have_content 'メモが更新されました'
-    expect(page).to have_content '編集した名前'
-  end
-
-  it 'メモを削除できること' do
-    create(:memo, name: 'ど忘れしやすい名前', user:)
-
-    visit root_path
-    expect(page).to have_content 'ど忘れしやすい名前'
-
-    # スワイプしてリンクを表示させる
-    slider = find '.swiper-wrapper'
-    execute_script("arguments[0].style.transform = 'translateX(-85.5px)';", slider)
-
-    delete_icon = find '.fa-trash-can'
-    delete_icon.click
-    expect(page.accept_confirm).to eq '本当に削除してよろしいですか？'
-    expect(page).to have_content 'メモが削除されました'
-    expect(page).to have_content 'まだメモが登録されていません'
-  end
-
-  it 'メモの内容を表示できること' do
-    create(:memo, content: 'ど忘れしやすい名前です', user:)
-
-    visit root_path
-    comment_icon = find '.fa-comment-dots'
-    comment_icon.click
-    expect(page).to have_content 'ど忘れしやすい名前です'
+    it 'ソートのリンクが機能すること' do
+      expect(first('#name').text).to eq 'ど忘れしやすい名前'
+      click_link('更新順')
+      find 'turbo-frame[complete]'
+      expect(first('#name').text).to eq '覚えておきたい名前'
+      click_link('ど忘れが多い順')
+      find 'turbo-frame[complete]'
+      expect(first('#name').text).to eq 'ど忘れしやすい名前'
+    end
   end
 end
