f1f7a9d0f0d481f8c31fe3600dcebd05eb756279
tominaga <aurora25sh.50nonono@gmail.com>
2024-02-01 17:27:59 +0900

    名前をtargetに変更した

--- app/controllers/memos/searches_controller.rb
+++ app/controllers/memos/searches_controller.rb
@@ -4,8 +4,8 @@ class Memos::SearchesController < ApplicationController
   def index
     memos = current_user.memos
     query = params[:query]
-    option = params[:search_option]
-    @memos = (query.present? ? memos.search(query, option) : memos).forgetful.page(params[:page])
+    target = params[:target]
+    @memos = (query.present? ? memos.search(query, target) : memos).forgetful.page(params[:page])
     @memos.each(&:increase_search_count) if query.present?
   end
 end
