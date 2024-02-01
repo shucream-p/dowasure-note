009cd01f1d518a92b9dc9d6ddc20bf5f7be94908
tominaga <aurora25sh.50nonono@gmail.com>
2024-02-01 15:21:52 +0900

    Memos::Searches#indexをリファクタリングした

--- app/controllers/memos/searches_controller.rb
+++ app/controllers/memos/searches_controller.rb
@@ -3,15 +3,9 @@
 class Memos::SearchesController < ApplicationController
   def index
     memos = current_user.memos
-    @memos = if params[:query].present?
-               memos.search(params[:query], params[:search_option])
-                    .forgetful
-                    .page(params[:page])
-             else
-               memos.forgetful
-                    .page(params[:page])
-             end
-
-    @memos.each(&:increase_search_count) if params[:query].present?
+    query = params[:query]
+    option = params[:search_option]
+    @memos = (query.present? ? memos.search(query, option) : memos).forgetful.page(params[:page])
+    @memos.each(&:increase_search_count) if query.present?
   end
 end
