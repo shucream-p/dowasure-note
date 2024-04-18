39e0dedbab7bb6e3667cb6c7285d6b995b082375
tominaga <aurora25sh.50nonono@gmail.com>
2024-04-18 14:29:22 +0900

    tagsをローカル変数に修正した

--- app/controllers/api/tags/searches_controller.rb
+++ app/controllers/api/tags/searches_controller.rb
@@ -3,9 +3,9 @@
 class API::Tags::SearchesController < ApplicationController
   def index
     query = params[:query]
-    @tags = ActsAsTaggableOn::Tag.for_tenant(current_user.id)
-                                 .where('name LIKE ?', "#{query}%")
-                                 .map(&:name)
-    render json: @tags
+    tags = ActsAsTaggableOn::Tag.for_tenant(current_user.id)
+                                .where('name LIKE ?', "#{query}%")
+                                .map(&:name)
+    render json: tags
   end
 end
